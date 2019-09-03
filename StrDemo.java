0c3fc855ab984e5b9a3b5f9132c8a9576c3e09a6
Nikita <oakjpc@gmail.com>
2019-09-03 21:31:25 +0300

    DdynInit.class DynInit.class DynInit.java LogicalOpTable.class LogicalOpTable.java LogicalOpTable01.class LogicalOpTable01.java ModDemo.class ModDemo.java RelLogOps.class RelLogOps.java SCops.class SCops.java ScopeDemo.class ScopeDemo.java SideEffects.class SideEffects.java StrDemo.class StrDemo.java VarInitDemo.class VarInitDemo.java

---
+++ StrDemo.java
@@ -0,0 +1,8 @@
+// Demonstrate escape sequences in strings.
+class StrDemo {
+  public static void main(String agrs[]) {
+    System.out.println("First line\nSecond line");
+    System.out.println("A\tB\tC");
+    System.out.println("D\tE\tF");
+  }
+}
